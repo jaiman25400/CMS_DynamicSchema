8e1e8f670313c8b9e67a381bc4e902560ef4a118
Raghvendra Singh <Raghav26897@gmail.com>
2021-11-16 10:09:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
 const ModelRouter = require("./routes/Model");
 
 const app = express();
 
-mongoose.connect( `mongodb+srv://user:User%40123@cluster0.m1fvc.mongodb.net/CMS-1?authSource=admin&replicaSet=atlas-116p88-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`, { useNewUrlParser: true })
-    .then(response => {
-        console.log("MongoDB Connected Successfully.");
-    }).catch(err => {
-        console.log("Database connection failed.", err);
-    });
+mongoose
+  .connect(
+    `mongodb+srv://Jaiman25:Jaiman%407567@cluster0.cbkdf.mongodb.net/test`,
+    { useNewUrlParser: true }
+  )
+  .then((response) => {
+    console.log("MongoDB Connected Successfully.");
+  })
+  .catch((err) => {
+    console.log("Database connection failed.", err);
+  });
 
-    app.use(express.json());
+app.use(express.json());
 
-    app.use("/model", ModelRouter);
+app.use("/model", ModelRouter);
 
-    app.listen(5000, () => {
-        console.log(`Server is running on Port ${5000}`);
-    });
+app.listen(5000, () => {
+  console.log(`Server is running on Port ${5000}`);
+});
