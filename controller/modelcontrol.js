167483244779429c6ecee6e1885ac799416fff01
Raghvendra Singh <Raghav26897@gmail.com>
2021-11-16 15:27:01 +0530

    updated many things

--- controller/modelcontrol.js
+++ controller/modelcontrol.js
@@ -6,7 +6,7 @@ const { body } = require("express-validator")
 
 module.exports.getallModel = async (req, res) => {
   console.log('Res gell all user')
-  return await modelModel.find({});
+  res.send( await modelModel.find({}));
   // await Student.find()
 };
 
@@ -19,4 +19,40 @@ module.exports.createModel = async (req, res) => {
   res.send("Created Model Successfully")
 };
 
+module.exports.getModelByName = async (req, res) => {
+  try {
+      const modelName = req.params.name
+      const model = await modelModel.findOne({ name: modelName });
+      if (!modelName) {
+          res.sendStatus(404);
+      }
+      res.send(model);
+  } catch {
+      res.sendStatus(500);
+  }
+};
 
+module.exports.updateSchemaByName = async (req, res) => {
+  try {
+      const modelschema = JSON.parse(req.body.modelSchema)
+      const modelName = req.params.name
+      const oldSchema = await modelModel.findOne({ name: modelName });
+      if (Object.entries(oldSchema).length === 0) {
+          res.send("Model ${modelName}Dosen't exits..")
+      } else {
+          const properties = Object.assign(oldSchema.modelSchema.properties, modelschema.properties)
+          let required = oldSchema.modelSchema.required
+
+          if (modelschema.required.length !== 0) {
+              required = required.concat(modelschema.required)
+          }
+          await modelModel.findOneAndUpdate(
+              { name: modelName },
+              { $set: { "modelSchema.properties": properties, "modelSchema.required": required } },
+          );
+          res.send("Updated SuccessFully")
+      }
+  } catch {
+      res.sendStatus(400);
+  }
+}
