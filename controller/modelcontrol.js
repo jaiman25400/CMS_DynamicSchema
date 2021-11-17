31529a9e741966a2bb9cb8a5f9e2851ca1578fb7
jaiman25400 <jaimanjoshi25@gmail.com>
2021-11-17 11:34:59 +0530

    new

--- controller/modelcontrol.js
+++ controller/modelcontrol.js
@@ -1,58 +1,74 @@
 const Student = require("../schema/ModelSchema");
 const validation = require("../middleware/validator");
+const docModel = require("../schema/docsSchema").docsModel;
 const modelModel = require("../schema/ModelSchema").modelModel;
 // import { body } from "express-validator";
-const { body } = require("express-validator")
+const { body } = require("express-validator");
 
 module.exports.getallModel = async (req, res) => {
-  console.log('Res gell all user')
-  res.send( await modelModel.find({}));
+  console.log("Res gell all user");
+  res.send(await modelModel.find({}));
   // await Student.find()
 };
 
 module.exports.createModel = async (req, res) => {
-  const name = req.body.name
-  const modelSchema =req.body.modelSchema
+  const name = req.body.name;
+  const modelSchema = req.body.modelSchema;
 
   const doc = await modelModel.create({ name, modelSchema: modelSchema });
   await doc.save();
-  res.send("Created Model Successfully")
+  res.send("Created Model Successfully");
 };
 
 module.exports.getModelByName = async (req, res) => {
   try {
-      const modelName = req.params.name
-      const model = await modelModel.findOne({ name: modelName });
-      if (!modelName) {
-          res.sendStatus(404);
-      }
-      res.send(model);
+    const modelName = req.params.name;
+    const model = await modelModel.findOne({ name: modelName });
+    if (!modelName) {
+      res.sendStatus(404);
+    }
+    res.send(model);
   } catch {
-      res.sendStatus(500);
+    res.sendStatus(500);
   }
 };
 
 module.exports.updateSchemaByName = async (req, res) => {
   try {
-      const modelschema = JSON.parse(req.body.modelSchema)
-      const modelName = req.params.name
-      const oldSchema = await modelModel.findOne({ name: modelName });
-      if (Object.entries(oldSchema).length === 0) {
-          res.send("Model ${modelName}Dosen't exits..")
-      } else {
-          const properties = Object.assign(oldSchema.modelSchema.properties, modelschema.properties)
-          let required = oldSchema.modelSchema.required
-
-          if (modelschema.required.length !== 0) {
-              required = required.concat(modelschema.required)
-          }
-          await modelModel.findOneAndUpdate(
-              { name: modelName },
-              { $set: { "modelSchema.properties": properties, "modelSchema.required": required } },
-          );
-          res.send("Updated SuccessFully")
+    const modelschema = req.body.modelSchema;
+    const modelName = req.params.name;
+    const oldSchema = await modelModel.findOne({ name: modelName });
+    if (Object.entries(oldSchema).length === 0) {
+      res.send(`Model ${modelName}Dosen't exits..`);
+    } else {
+      const properties = Object.assign(
+        oldSchema.modelSchema.properties,
+        modelschema.properties
+      );
+      let required = oldSchema.modelSchema.required;
+      console.log("Required: ", required);
+      if (modelschema.required) {
+        required = required.concat(modelschema.required);
       }
+      await modelModel.findOneAndUpdate(
+        { name: modelName },
+        {
+          $set: {
+            "modelSchema.properties": properties,
+            "modelSchema.required": required,
+          },
+        }
+      );
+      res.send("Updated SuccessFully");
+    }
   } catch {
-      res.sendStatus(400);
+    console.log("here bad");
+    res.sendStatus(400);
   }
-}
+};
+module.exports.deleteModel = async (req, res) => {
+  const modelId = req.params.modelId;
+  await modelModel.findByIdAndDelete(modelId);
+  await docModel.deleteMany({ model: modelId });
+  res.send("Document Deleted Sucessfully");
+};
--- routes/Model.js
+++ routes/Model.js
@@ -7,8 +7,14 @@ router.get("/", StudC.getallModel);
 
 router.post("/createModel", StudC.createModel);
 
-// router.put("/:id", StudC.updateModel);
+//Get Specific Model
+router.get("/readModel/:name", StudC.getModelByName);
+
+//Update Model With Schema
+router.put("/updateModel/:name", StudC.updateSchemaByName)
 
 // router.delete("/:id", StudC.deleteModel);
+// Delete Model From Database
+router.delete("/deleteModel/:modelId", StudC.deleteModel)
 
 module.exports = router;
