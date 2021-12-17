4a5ba184c996ec486779aab6897307d00a2b9be5
jaiman25400 <jaimanjoshi25@gmail.com>
2021-12-17 14:12:08 +0530

    d

--- controller/mediaController.js
+++ controller/mediaController.js
@@ -1,22 +1,22 @@
 const mediaModel = require("../schema/mediaSchema").mediaModel;
 
-module.exports.getAllMedia = async (req,res) => {
+module.exports.getAllMedia = async (req, res) => {
   const Forms = await mediaModel.find({});
-  res.send(Forms)
-}
+  res.send(Forms);
+};
 
-module.exports.getMediaByNameAndId = async (req,res) => {
-  const name = req.params.name
-  console.log('Name:',name)
-  const data = await mediaModel.findOne({ name:name })
-  console.log('dataaa :',data)
-  res.send(data)
-}
+module.exports.getMediaByNameAndId = async (req, res) => {
+  const name = req.params.name;
+  console.log("Name:", name);
+  const data = await mediaModel.findOne({ name: name });
+  console.log("dataaa :", data);
+  res.send(data);
+};
 
 module.exports.addMediaData = async (req, res) => {
-  console.log("Media", req.body);
+  console.log("Media", req);
   const data = req.body;
-  const name = req.body.Title
+  const name = req.body.Title;
   if (!req.files) {
     res.send("Enter File Name");
     res.status(500);
@@ -25,15 +25,40 @@ module.exports.addMediaData = async (req, res) => {
     const fileName = file.name;
     console.log("file", file);
     console.log("file Name", fileName);
-    file.mv(`public/Media/${file.name}`, (err) => {
-      if (err) {
-        console.log("erre", err);
-        return res.status(500).send(err);
-      } else {
-        console.log("save");
+    if (file.length > 0) {
+      for (let i = 0; i < file.length; i++) {
+        console.log('File Name is :',file[i].name, i,file[i])
+        file[i].mv(`public/Media/${file[i].name}`, (err) => {
+          if (err) {
+            console.log("erre", err);
+            return res.status(500).send(err);
+          } else {
+            console.log("save");
+          }
+        });
+        await mediaModel.create({
+          mediaSchema: data,
+          fileName: file[i].name,
+          name: file[i].name,
+        });
       }
-    });
-    await mediaModel.create({ mediaSchema: data, fileName: fileName,name:name });
-    res.send("Document Created SuccesFully");
+
+      res.send("Document Created SuccesFully");
+    } else {
+      file.mv(`public/Media/${file.name}`, (err) => {
+        if (err) {
+          console.log("erre", err);
+          return res.status(500).send(err);
+        } else {
+          console.log("save");
+        }
+      });
+      await mediaModel.create({
+        mediaSchema: data,
+        fileName: fileName,
+        name: name,
+      });
+      res.send("Document Created SuccesFully");
+    }
   }
 };
